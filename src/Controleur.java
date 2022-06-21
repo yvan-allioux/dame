17d49b4a77926fdc4744a666fd8327d7c0b8470f
apolline18 <62940019+apolline18@users.noreply.github.com>
2022-06-21 21:38:14 +0200

    Update Controleur.java

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,8 +1,4 @@
-import javax.swing.*;
-
 public class Controleur {
-    //attention le controleur est juste une interface entre le model et la vue dans le cadre de ce projet !!!
-
     // ATTRIBUTS //
     private ModelJoueur joueur1;
     private ModelJoueur joueur2;
@@ -11,11 +7,9 @@ public class Controleur {
     private FenetreJeu vueJeu;
     private FenetrePseudo vuePseudo;
     // CONSTRUCTEUR //
-    public Controleur(ModelJoueur unJoueur1, ModelJoueur unJoueur2, ModelTerrain mTerrainParam, FenetreJeu vueJeuParam) {
+    public Controleur(ModelJoueur unJoueur1, ModelJoueur unJoueur2){
         joueur1 = unJoueur1;
         joueur2 = unJoueur2;
-        this.mTerrain = mTerrainParam;
-        this.vueJeu = vueJeuParam;
     }
     // METHODES //
     public void enregistrerPseudo(String pseudo1, String pseudo2)
@@ -24,13 +18,10 @@ public class Controleur {
 
         joueur1.setPseudo(pseudo1);
         joueur2.setPseudo(pseudo2);
+
         System.out.println(joueur1.getPseudo() + " - " +joueur2.getPseudo());
-
-            vueJeu.setLabelJ1("Joueur 1 : "+joueur1.getPseudo());
-
-            vueJeu.setLabelJ2("Joueur 2 : "+joueur2.getPseudo());
-
-
+        vueJeu.setLabelJ1("Joueur 1 : "+joueur1.getPseudo());
+        vueJeu.setLabelJ2("Joueur 2 : "+joueur2.getPseudo());
 
     }
 }
